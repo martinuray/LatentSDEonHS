9ca69cfb06e4f403226c7c29c1ea7d84195f4273
Martin Uray <martin.uray@fh-salzburg.ac.at>
2025-02-05 14:19:32 +0100

    updated data generation

--- generate_very_simple_data.py
+++ generate_very_simple_data.py
@@ -1,89 +1,91 @@
 ##%
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import torch
 
+# stuff for signals
 base_frequency = 1.0   # Hz
 fs = 1 / (10*10**-3)   # Hz; every 10ms; as the idea with the testbed
 step_size = 1/fs
-
-num_periods = 1
-
 start_s = 0
-end_s = 1/base_frequency * num_periods
+end_s = 1/base_frequency
 
-num_features = 1
+# stuff for the dataset
+num_periods = 1
+num_features = 2
 
 #%%
 x = np.arange(0, end_s, step_size)
-Y = np.zeros((num_features, len(x)))
+Y = np.zeros((num_periods, len(x), num_features))
+
 
 def norm_data(y_):
+    min_vals = y_.min(axis=1, keepdims=True)
+    max_vals = y_.max(axis=1, keepdims=True)
+    n_arr = (y_ - min_vals) / (max_vals - min_vals)
     # in the basic sine example the data is in [-0.8, 0.8]
-    return (2. * (y_ - np.min(y_)) / np.ptp(y_) - 1) * 0.8
+    return n_arr * 0.8
 
-Y[0] = 1.00 * np.sin(2*np.pi*base_frequency*1.*x) #+ 0.21 * np.sin(2*np.pi*base_frequency*2*x)
+
+Y[:, :, 0] = 1.00 * np.sin(2*np.pi*base_frequency*1.*x) #+ 0.21 * np.sin(2*np.pi*base_frequency*2*x)
+
 if num_features > 1:
-    Y[1] = 0.30 * np.cos(2*np.pi*base_frequency*4.*x) + 0.21 * np.sin(2*np.pi*base_frequency*2*x)
+    Y[:, :, 1] = 0.30 * np.cos(2*np.pi*base_frequency*4.*x) + 0.21 * np.sin(2*np.pi*base_frequency*2*x)
 if num_features > 2:
-    Y[2] = 0.45 * np.cos(2*np.pi*base_frequency*16/4*x) + 0.21 * np.sin(2*np.pi*base_frequency*4/4*x)
+    Y[:, :, 2] = 0.45 * np.cos(2*np.pi*base_frequency*16/4*x) + 0.21 * np.sin(2*np.pi*base_frequency*4/4*x)
 if num_features > 3:
-    Y[3] = 0.40 * np.cos(2*np.pi*base_frequency*18/4*x) + 0.21 * np.sin(2*np.pi*base_frequency*8/4*x)
+    Y[:, :, 3] = 0.40 * np.cos(2*np.pi*base_frequency*18/4*x) + 0.21 * np.sin(2*np.pi*base_frequency*8/4*x)
 
-for i in range(num_features):
-    Y[i] = norm_data(Y[i])
+# normalize Y, but over the second axis only
+Y = norm_data(Y)
+Y_masked = Y.copy()
+
+# roughly 30 % of the samples are non-nan
+ratio_of_not_sampling = 1 - 30/100 # as with the sine example
+number_of_samples = int(x.shape[0] * ratio_of_not_sampling)
+
+mask = np.random.rand(num_periods, x.shape[0], num_features) < ratio_of_not_sampling
+Y_masked[mask] = 0.
 
 #%%
 plt.figure()
-for i in range(num_features):
-    plt.plot(x, Y[i])
-
+for idx in range(num_features):
+    y_m = Y_masked[0, :, idx][~mask[0, :, idx]]
+    x_m = x[~mask[0, :, idx]]
+    plt.plot(x, Y[0, :, idx], '.', label=f"GT Ft. {idx}")
+    plt.plot(x_m, y_m, '.', label=f"Subsampled Ft. {idx}")
 plt.grid()
+plt.legend()
 plt.show()
 
-#%% verify the periodicity we expect
-n_samples_per_period = int(fs * 1 / base_frequency)
-for feature in range(num_features):
-    a = Y[feature].reshape(n_samples_per_period, -1)
-    plt.plot(np.arange(n_samples_per_period), a)
-    plt.show()
+#%% convert to tensor structure
+periods_to_store = []
 
-#%%
-min_sample = 0.75
-max_sample = 0.15
+for per in range(num_periods):
+    vals = Y[per]
+    m = ~mask[per]
+    tt = x[m.any(axis=1)]
+    vals = vals[m.any(axis=1)]
+    m = m[m.any(axis=1)] * 1
 
-for feature in range(num_features):
-    ratio_of_not_sampling = np.random.random_sample() * (max_sample - min_sample) + min_sample
-    ratio_of_not_sampling = 1 - 30/100 # as with the sine example
+    if tt[0] != 0.0:
+        tt = np.insert(tt, 0, 0.0)
+        vals = np.insert(vals, 0, np.zeros(vals.shape[1]), axis=0)
+        m = np.insert(m, 0, np.zeros(m.shape[1]), axis=0)
 
-    idxs_to_nan = np.random.choice(Y[feature].shape[0],
-                                   int(Y[feature].shape[0] * ratio_of_not_sampling),
-                                   replace=False)
+    vals = torch.tensor(vals)
+    tt = torch.tensor(tt)
+    m = torch.tensor(m)
 
-    Y[feature][idxs_to_nan] = np.nan
+    periods_to_store.append((per, tt, vals, m))
 
-#%% check on "sampled" data
-n_samples_per_period = int(fs * 1 / base_frequency)
-for feature in range(num_features):
-    a = Y[feature].reshape(n_samples_per_period, -1)
-    plt.plot(np.arange(n_samples_per_period), a[:, 0], '.')
-    plt.show()
+# testing the shapes before storing
+for _, tt_, vals_, m_ in periods_to_store:
+    assert tt_.shape[0] == vals_.shape[0]
+    assert m_.shape == vals_.shape
 
-#%%
-import pandas as pd
-df = pd.DataFrame(data=Y.T,    # values
-                  index=x,    # 1st column as index
-                  columns=np.arange(0,num_features))  # 1st row as the column names
-
-sp_ = df.shape[0]
-df_train_, df_test_ = df.iloc[:sp_], df.iloc[sp_:]
-
-def store_data(df_, dataset_label='train'):
-    for idx in range(num_periods):
-        df_i = df_.iloc[idx * n_samples_per_period:(idx + 1) * n_samples_per_period]
-        df_i.index = df_i.index - df_i.index[0]
-        df_i.to_csv(f'data_dir/BasicData/raw/sample_{dataset_label}_num-ft_{num_features}_it_{idx}.csv')
-
-store_data(df_train_, 'train')
-#store_data(df_test_, 'test')
-
-#%%
+torch.save(
+    periods_to_store,
+    os.path.join(f'data_dir/BasicData/processed/basic_data_num-ft_{num_features}.pt')
+)
