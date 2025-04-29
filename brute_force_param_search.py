867d5af3b25323069dfab40ec635462deec880ff
Martin Uray <martin.uray@fh-salzburg.ac.at>
2025-04-29 11:03:24 +0200

    set param for gridsearch

--- brute_force_param_search.py
+++ brute_force_param_search.py
@@ -6,18 +6,18 @@ from basic_data_anomaly_detection import start_experiment, extend_argparse
 from data.ad_provider import ADProvider
 from utils.parser import generic_parser
 
-params = {
-    'lr' : [10e-5, 10e-6],
-    'kl0_weight' : [10e-1, 10e-2, 10e-3],
-    'klp_weight' : [10e-1, 10e-2, 10e-3],
-    'pxz_weight' : [1],
-    'z_dim' : [4, 8],
-    'h_dim' : [16, 32],
-    'n_deg' : [4, 6, 8],
-    'use_atanh' : [True, False],
-    'non_linear_decoder': [True, False],
-    }
 
+params = {
+    #'lr' : [1e-3, 1e-4], # set different for each dataset
+    'use_atanh': [False],
+    'non_linear_decoder': [True],
+    'z_dim' : [8],  # experiments 26.-27.4.
+    'kl0_weight' : [1e-1, 1e-2, 1e-3],  # 3
+    'klp_weight' : [1e-1, 1e-2, 1e-3],  # 3
+    'pxz_weight' : [1],
+    'h_dim' : [16, 32],                  # 2
+    'n_deg' : [4, 6, 8],                 # 2
+}
 
 
 def generate_param_combinations(params_):
@@ -31,15 +31,18 @@ def main():
     args_ = parser.parse_args()
     args_dict_ = vars(args_)
 
+    if args_.dataset == "SMD":
+        args_.lr = 1e-4
+
     provider = ADProvider(data_dir='data_dir', dataset=args_.dataset, n_samples=1000 if args_.debug else None)
 
 
     os.makedirs('runs/', exist_ok=True)
 
     combinations = generate_param_combinations(params)
-    print(f"Generated {len(combinations)} combinations")
+    print(f"Starting grid search with {len(combinations)} combinations")
 
-    for combo in combinations:  # Print only first 5 combinations for preview
+    for combo in combinations:
 
         for key_, value_ in combo.items():
             if key_ not in args_dict_.keys():
